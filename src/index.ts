58d71c0d03911621f6d4441a09c2bf4196a3dfdc
Evan Shapiro <evanshapiro@Evans-Work-MacBook-Pro.local>
2022-04-22 21:24:50 +0200

    start project

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,82 @@
-import { Field, SmartContract, state, State, method, UInt64 } from 'snarkyjs';
+import {
+  Field,
+  state,
+  State,
+  method,
+  UInt64,
+  PrivateKey,
+  SmartContract,
+  Mina,
+  Party,
+  isReady,
+  Bool,
+  Permissions,
+  shutdown,
+} from 'snarkyjs';
 
-/**
- * Basic Example
- * See https://docs.minaprotocol.com/zkapps for more info.
- *
- * The Add contract initializes the state variable 'num' to be a Field(1) value by default when deployed.
- * When the 'update' method is called, the Add contract adds Field(2) to its 'num' contract state.
- */
-export default class Add extends SmartContract {
-  @state(Field) num = State<Field>();
+await isReady;
 
-  // initialization
-  deploy(initialBalance: UInt64, num: Field = Field(1)) {
-    super.deploy();
-    this.balance.addInPlace(initialBalance);
-    this.num.set(num);
+// =======================================================
+
+async function main() {
+  let Local = Mina.LocalBlockchain();
+  Mina.setActiveInstance(Local);
+
+  let account1 = Local.testAccounts[0].privateKey;
+
+  let zkappKey = PrivateKey.random();
+  let zkappAddress = zkappKey.toPublicKey();
+
+  let initialBalance = UInt64.fromNumber(10_000_000_000);
+  let initialState = Field(1);
+
+  console.log('deploy');
+  Local.transaction(account1, () => {
+    const p = Party.createSigned(account1, { isSameAsFeePayer: true });
+    p.balance.subInPlace(initialBalance);
+    let zkapp = new SimpleZkapp(zkappAddress);
+    zkapp.deploy({ zkappKey, initialBalance, initialState });
+  }).send();
+
+  let zkappState = (await Mina.getAccount(zkappAddress)).zkapp.appState[0];
+  console.log('initial state: ' + zkappState);
+
+  console.log('update');
+  Local.transaction(account1, async () => {
+    let zkapp = new SimpleZkapp(zkappAddress);
+    zkapp.update(Field(3));
+    // TODO: mock proving
+    zkapp.self.sign(zkappKey);
+    zkapp.self.body.incrementNonce = Bool(true);
+  }).send();
+
+  zkappState = (await Mina.getAccount(zkappAddress)).zkapp.appState[0];
+  console.log('final state: ' + zkappState);
+
+  shutdown();
+}
+
+class SimpleZkapp extends SmartContract {
+  @state(Field) x = State<Field>();
+
+  deploy(args: {
+    zkappKey: PrivateKey;
+    initialBalance: UInt64;
+    initialState: Field;
+  }) {
+    super.deploy(args);
+    this.self.update.permissions.setValue({
+      ...Permissions.default(),
+      editState: Permissions.proofOrSignature(),
+    });
+    this.balance.addInPlace(args.initialBalance);
+    this.x.set(args.initialState);
   }
 
-  @method async update() {
-    const currentState = await this.num.get();
-    const newState = currentState.add(2);
-    newState.assertEquals(currentState.add(2));
-    this.num.set(newState);
+  @method update(y: Field) {
+    let x = this.x.get();
+    this.x.set(x.add(y));
   }
 }
+
+main();
